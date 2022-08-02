f8edd2b9e59def86356db0e3bbe191fa7fc53a55
Sumit <sppjain8055@gmail.com>
2022-08-02 13:48:07 +0530

    Adding display function in project

--- GitTest.java
+++ GitTest.java
@@ -2,10 +2,8 @@ package com.persistent.git;
 
 public class GitTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+	public void display(){
+}
 
-	}
 
 }
